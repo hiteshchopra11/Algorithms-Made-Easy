c70ad56b07fcbd0af193b9d848742da5cc688c45
Hitesh Chopra <34310832+hiteshchopra11@users.noreply.github.com>
2021-05-04 21:46:14 +0530

    Update and rename solutions/589. N-ary Tree Preorder Traversal.cpp to May4/589. N-ary Tree Preorder Traversal.cpp

--- May4/589. N-ary Tree Preorder Traversal.cpp
+++ May4/589. N-ary Tree Preorder Traversal.cpp
@@ -4,20 +4,20 @@ class Node {
 public:
     int val;
     vector<Node*> children;
-​
+
     Node() {}
-​
+
     Node(int _val) {
         val = _val;
     }
-​
+
     Node(int _val, vector<Node*> _children) {
         val = _val;
         children = _children;
     }
 };
 */
-​
+
 class Solution {
 public:
     vector<int> preorder(Node* root) {
